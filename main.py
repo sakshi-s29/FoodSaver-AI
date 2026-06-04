7fff942989d8229b26f37cccd8f1ad3264ea26e8
sakshi-s29 <sakshishripad29@gmail.com>
2026-06-04 13:40:51 +0530

    Add smart expiry recommendation system

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 
 def add_food():
     food_name = input("Enter Food Name: ")
@@ -11,11 +12,58 @@ def add_food():
 
     print("Food Item Added Successfully!")
 
+def view_food():
+    try:
+        with open("food_data.csv", "r") as file:
+            reader = csv.reader(file)
+
+            print("\nFood Items:")
+            print("---------------------------")
+
+            for row in reader:
+                print(f"Food: {row[0]}")
+                print(f"Quantity: {row[1]}")
+                print(f"Expiry Date: {row[2]}")
+                print("---------------------------")
+
+    except FileNotFoundError:
+        print("No food items found.")
+
+def recommend_food():
+    try:
+        with open("food_data.csv", "r") as file:
+            reader = csv.reader(file)
+
+            print("\nSmart Recommendations")
+            print("---------------------------")
+
+            for row in reader:
+                food = row[0]
+                expiry = row[2]
+
+                expiry_date = datetime.strptime(expiry, "%Y-%m-%d")
+                today = datetime.today()
+
+                days_left = (expiry_date - today).days
+
+                if days_left <= 1:
+                    recommendation = "Use Immediately"
+                elif days_left <= 3:
+                    recommendation = "Consume Soon"
+                else:
+                    recommendation = "Fresh"
+
+                print(f"{food} -> {recommendation}")
+
+    except FileNotFoundError:
+        print("No food data found.")
+
 while True:
     print("\n===== FoodSaver AI =====")
     print("1. Add Food Item")
     print("2. View Food Items")
-    print("3. Exit")
+    print("3. Smart Recommendations")
+    print("4. Exit")
 
     choice = input("Enter choice: ")
 
@@ -23,10 +71,13 @@ while True:
         add_food()
 
     elif choice == "2":
-        print("View Food Items")
+        view_food()
 
     elif choice == "3":
-        break
+        recommend_food()
 
+    elif choice == "4":
+        break
+        
     else:
         print("Invalid Choice")
